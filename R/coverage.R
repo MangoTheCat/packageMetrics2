c433eb92ad53c075789617ae25c7250134092dbc
Gabor Csardi <csardi.gabor@gmail.com>
2016-04-22 13:56:18 +0100

    Ignore errors and warnings in coverage calculation
    But report NA.

--- R/coverage.R
+++ R/coverage.R
@@ -5,10 +5,15 @@
 author_test_coverage <- function(pkg, by = c("line", "expression")) {
 
   message("Calculating test coverage for ", pkg)
-  cov <- with_envvar(
-    c("R_LIBS_USER" = paste(.libPaths(), collapse = .Platform$path.sep)),
-    package_coverage(pkg)
+  tryCatch(
+    {
+      cov <- with_envvar(
+        c("R_LIBS_USER" = paste(.libPaths(), collapse = .Platform$path.sep)),
+        package_coverage(pkg)
+      )
+      percent_coverage(cov, by = by)
+    },
+    error = function(e) NA_real_,
+    warning = function(w) TRUE
   )
-
-  percent_coverage(cov, by = by)
 }
