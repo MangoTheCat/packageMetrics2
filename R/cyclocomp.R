e2298f0ec1b4e107eea2b0f0867a0ebcea35e327
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-19 13:19:01 +0100

    tryCatch for cyclocomp, it fails occasionally

--- R/cyclocomp.R
+++ R/cyclocomp.R
@@ -2,6 +2,10 @@
 #' @importFrom cyclocomp cyclocomp_package
 
 mean_cyclocomp <- function(package, version = NULL) {
-  cc <- cyclocomp_package(package)
+  ## It fails on some packages, e.g. BiocInstaller
+  cc <- tryCatch(
+    cyclocomp_package(package),
+    error = function(e) list(cyclocomp = NA)
+  )
   mean(cc$cyclocomp)
 }
